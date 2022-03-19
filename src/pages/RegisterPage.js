5e0c8e7e7659460e3b19d6d0f46121d61494e5a0
Dominick7771 <60191808+Dominick7771@users.noreply.github.com>
2022-03-19 18:40:24 +0200

    Update RegisterPage.js

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,9 +1,5 @@
 import React from 'react';
-<<<<<<< HEAD
 import Header from "../components/header/Header";
-import Main from "../components/main/Main";
-=======
->>>>>>> 2411a6524c55c38023c1972eb7b9d98e2791aa11
 
 const RegisterPage = () => {
     return (
