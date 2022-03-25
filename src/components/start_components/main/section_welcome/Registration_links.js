db150350b16f70b1cd8130fceb481edad2abc85e
Anton Golub <jukonda23@gmail.com>
2022-03-25 20:38:11 +0300

    refactore links startPage

--- src/components/start_components/main/section_welcome/Registration_links.js
+++ src/components/start_components/main/section_welcome/Registration_links.js
@@ -2,20 +2,21 @@ import React from 'react';
 import style from '../../../../css_moduls/start_css/section_welcome.module.css'
 import search from '../../../../assets/png/search.png'
 import {useHistory} from "react-router-dom";
+import {useDispatch} from "react-redux";
 
 const RegistrationLinks = () => {
 
-    const {push} = useHistory()
+    const dispatch = useDispatch()
 
     return (
         <>
-            <button className={`${style.btnLostPet}`} onClick={() => push('/authorization')}>
+            <button className={`${style.btnLostPet}`} onClick={() => dispatch({type: 'SET_MODAL', payload: true})}>
                 <div className={`${style.search}`}>
                     <span className={`${style.btnSpan}`}>I lost my pet!</span>
                     <img src={search}/>
                 </div>
             </button>
-            <button className={`${style.btnFoundPet}`} onClick={() => push('/authorization')}>
+            <button className={`${style.btnFoundPet}`} onClick={() => dispatch({type: 'SET_MODAL', payload: true})}>
                 <span className={`${style.btnSpanWhite}`}>I found a pet!</span>
             </button>
         </>
