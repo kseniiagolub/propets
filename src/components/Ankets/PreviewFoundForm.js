819234042ec7a08c52494f1a0841b2feaab07a84
Golub Anton <jukonda23@gmail.com>
2022-03-30 15:15:29 +0300

    refactor

--- src/components/Ankets/PreviewFoundForm.js
+++ src/components/Ankets/PreviewFoundForm.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import {useDispatch, useSelector} from "react-redux";
+import style from "../../css_moduls/home_css/home.module.css";
+
+import src from '../../assets/png/preview.png'
 
 const PreviewFoundForm = () => {
 
@@ -7,11 +10,40 @@ const PreviewFoundForm = () => {
     const anket = useSelector(state => state.anketa)
 
     return (
-        <>
-          <h1>Preview</h1>
-            <img src={anket.images[3]}/>
+        <div className={`${style.mainWhiteBack} d-flex flex-column col-6 align-items-center overflow-auto mt-3`}>
+            <h1 className={`${style.titleSemiBold} col-10`}><span className={`${style.titleBold}`}>Preview and Publish. </span>Please share the post to your FB to be more effective</h1>
+            <div className={`${style.postCard} d-flex`}>
+                <div className={`col-5`}>
+                    <img className={`${style.imgPreview}`} src={src}/>
+                </div>
+                <div className={`col-7 ps-3 pe-3`}>
+                    <span className={`${style.titleBigBold}`}>Dog, Golden Retriever</span>
+                    <div>
+                        <div>
+                            <h2>Color: <span>golden</span></h2>
+                            <h2>Sex: <span>male</span></h2>
+                            <h2>Height: <span>45 cm</span></h2>
+                        </div>
+                        <div>
+                            <h2>Distinnctive features <span>golden</span></h2>
+                        </div>
+                    </div>
+                    <div>
+                        <h2>Description: <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ab ad adipisci, aliquid amet corporis delectus dolorum error esse et explicabo fuga harum hic impedit ipsa iusto magnam maiores quae quam qui quidem quod, quos recusandae sint, soluta temporibus totam veniam! Eligendi harum ipsa laudantium magni minus suscipit! Esse, explicabo.</span></h2>
+                    </div>
+                    <hr/>
+                    <div>
+                        <span>Florentin, 27, Tel Aviv</span>
+                    </div>
+                    <div>
+                        <div>
+                            <img/>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <button onClick={() => dispatch({type: 'SET_ANKET', payload: {finish: false, edit: false}})}>btn</button>
-        </>
+        </div>
     );
 };
 
--- src/components/home/home_main/middle_block_content/content_home/PostCard.js
+++ src/components/home/home_main/middle_block_content/content_home/PostCard.js
@@ -13,42 +13,41 @@ const PostCard = () => {
     let disp = dropdown ? 'd-block' : 'd-none'
 
     return (
-        <>
-            <div className={`${style.postCard} row d-flex`}>
-                <div className={`col-1`}>
-                    <img className={`${style.avatarImg}`} src={avatar} alt={'dog'}/>
-                </div>
-                <div className={`col-10`}>
-                    <h3 className={`${style.titleSemiBold}`}>Jonh Goodboi</h3>
-                    <p className={`${style.smallerText}`}>2 h</p>
-                    <img className={'mb-2 mt-1'} src={dog} alt={'dog'}/>
-                    <p className={`${style.smallerTextBlack} ${style.smallerTextBlackWidth}`}>Fox nymphs grab quick-jived waltz. Brick quiz whangs
-                        jumpy veldt fox. Bright vixens jump; dozy fowl quack
-                        <a className={`${style.greenLink}`} href={'#'}>…more</a></p>
-                </div>
-                <div className={`col-1 d-flex flex-column justify-content-between pb-3`}>
-                    <a href={'#'}><img className={`${style.dotsBtn}`} src={dots} alt={'dots'}
-                                       onClick={() => setDropdown(!dropdown)}/></a>
-
-                    <div className={`${style.dropdownContentGreen} ${disp} d-flex flex-column`}>
-                        <button className={`${style.navBtn} d-flex mb-2`}>
-                            <div className={`d-flex`}>
-                                <img className={`${style.iconBtnBlack}`} src={hide} alt={'hide'}/>
-                                <span className={`${style.smallerTextBlack}`}>Hide from feed</span>
-                            </div>
-                        </button>
-                        <button className={`${style.navBtn} d-flex mb-2`}>
-                            <div className={`d-flex`}>
-                                <img className={`${style.iconBtnBlack}`} src={close} alt={'unfollow'}/>
-                                <span className={`${style.smallerTextBlack}`}>Unfollow</span>
-                            </div>
-                        </button>
-                    </div>
-
-                    <a href={'#'}><img className={`${style.starBtn}`} src={star} alt={'star'}/></a>
-                </div>
+        <div className={`${style.postCard} row d-flex`}>
+            <div className={`col-1`}>
+                <img className={`${style.avatarImg}`} src={avatar} alt={'dog'}/>
             </div>
-        </>
+            <div className={`col-10`}>
+                <h3 className={`${style.titleSemiBold}`}>Jonh Goodboi</h3>
+                <p className={`${style.smallerText}`}>2 h</p>
+                <img className={'mb-2 mt-1'} src={dog} alt={'dog'}/>
+                <p className={`${style.smallerTextBlack} ${style.smallerTextBlackWidth}`}>Fox nymphs grab quick-jived
+                    waltz. Brick quiz whangs
+                    jumpy veldt fox. Bright vixens jump; dozy fowl quack
+                    <a className={`${style.greenLink}`} href={'#'}>…more</a></p>
+            </div>
+            <div className={`col-1 d-flex flex-column justify-content-between pb-3`}>
+                <a href={'#'}><img className={`${style.dotsBtn}`} src={dots} alt={'dots'}
+                                   onClick={() => setDropdown(!dropdown)}/></a>
+
+                <div className={`${style.dropdownContentGreen} ${disp} d-flex flex-column`}>
+                    <button className={`${style.navBtn} d-flex mb-2`}>
+                        <div className={`d-flex`}>
+                            <img className={`${style.iconBtnBlack}`} src={hide} alt={'hide'}/>
+                            <span className={`${style.smallerTextBlack}`}>Hide from feed</span>
+                        </div>
+                    </button>
+                    <button className={`${style.navBtn} d-flex mb-2`}>
+                        <div className={`d-flex`}>
+                            <img className={`${style.iconBtnBlack}`} src={close} alt={'unfollow'}/>
+                            <span className={`${style.smallerTextBlack}`}>Unfollow</span>
+                        </div>
+                    </button>
+                </div>
+
+                <a href={'#'}><img className={`${style.starBtn}`} src={star} alt={'star'}/></a>
+            </div>
+        </div>
     );
 };
 
